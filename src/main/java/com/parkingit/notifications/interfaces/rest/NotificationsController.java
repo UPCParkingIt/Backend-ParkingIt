b7bc6952245a4b99e22907dcafd19c2db26249f8
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2025-12-03 02:09:05 -0500

    fix: refactor notifications endpoint to use userId path variable

--- src/main/java/com/parkingit/notifications/interfaces/rest/NotificationsController.java
+++ src/main/java/com/parkingit/notifications/interfaces/rest/NotificationsController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE })
@@ -26,16 +27,15 @@ public class NotificationsController {
     private final NotificationQueryService queryService;
     private final ExternalIamService externalIamService;
 
-    @GetMapping()
-    public ResponseEntity<List<NotificationResource>> getAllNotificationsByProfileId(@AuthenticationPrincipal UserDetails userDetails) {
-        var email = userDetails.getUsername();
-        var userId = externalIamService.fetchUserIdByEmail(email);
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<List<NotificationResource>> getAllNotificationsByProfileId(@PathVariable UUID userId) {
+        var userResult = externalIamService.fetchUserById(userId);
 
-        if (userId == null) {
-            throw new IllegalArgumentException("Profile ID not found for the provided email");
+        if (userResult == null) {
+            throw new IllegalArgumentException("User not found for the provided ID");
         }
 
-        var getAllNotificationsQuery = new GetAllNotificationsByUserIdQuery(userId);
+        var getAllNotificationsQuery = new GetAllNotificationsByUserIdQuery(userResult.getId());
         var notifications = queryService.handle(getAllNotificationsQuery);
         if (notifications.isEmpty()) {
             return ResponseEntity.noContent().build();
